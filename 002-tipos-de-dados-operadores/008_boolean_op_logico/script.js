0925a44539d13105e5d4cc4bd2dbbc4d87dbde5d
HgPBrito <hgbrito.tr@gmail.com>
2022-05-05 18:03:13 -0300

    booleans e conversoes automaticas

---
+++ 002-tipos-de-dados-operadores/008_boolean_op_logico/script.js
@@ -0,0 +1,11 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 < 3 || 3 == 1 );
+
+console.log(3 == 3 && 'igual' == 'igual');
+console.log('diferente' == 'igual' || false);
+
+console.log(!(true && true));
+console.log(!(!(true && true)));
+
+console.log(true && true);
+console.log(false || false);
---
+++ 002-tipos-de-dados-operadores/009_boolean_op_ternario/script.js
@@ -0,0 +1,4 @@
+console.log(5 > 2 ? 'É sim!' : 'É não!');
+console.log(false ? 5 : 4 );
+console.log('igual' == 'igual' ? 'É igual!' : 'Não é o igual!');
+console.log('diferente' == 'igual' ? 'É igual!' : 'Não é o igual!');
