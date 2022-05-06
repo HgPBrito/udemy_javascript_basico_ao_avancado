e19e43dd034b36d7ffd9085b7ccf7e28b236064d
HgPBrito <hgbrito.tr@gmail.com>
2022-05-06 18:00:07 -0300

    exercicios de tipo de dados

---
+++ 003-exercicios-tipos-de-dados/001_exercicios_strings/script.js
@@ -0,0 +1,3 @@
+console.log("Oi, teste com String!");
+console.log('Agora usando aspas simples!');
+console.log(`Teste com crases, template literal`);
---
+++ 003-exercicios-tipos-de-dados/004_exercicios_operadores_logicos/script.js
@@ -0,0 +1,3 @@
+console.log(2 > 1 && 3 === 3);
+console.log(3 > 5 || 5 > 2);
+console.log(!true);
