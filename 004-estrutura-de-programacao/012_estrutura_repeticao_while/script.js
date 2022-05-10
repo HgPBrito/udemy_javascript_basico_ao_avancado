fef97e31d6116b413b26b9893db3c5c217b28566
HgPBrito <hgbrito.tr@gmail.com>
2022-05-10 18:24:29 -0300

    estruturas de repeticao

--- 004-estrutura-de-programacao/012_estrutura_repeticao_while/script.js
+++ 004-estrutura-de-programacao/012_estrutura_repeticao_while/script.js
@@ -1,6 +1,13 @@
 let x = 10;
 
-while (x > 10) {
-    console.log(x);
+while (x > 0) {
+    console.log(`O x agora é ${x}.`);
     x = x - 1;
+}
+
+let y = 0;
+
+while (y <= 10) {
+    console.log(`O x agora é ${y}.`);
+    y = y + 1;
 }
---
+++ 004-estrutura-de-programacao/013_estrutura_repeticao_do_while/script.js
@@ -0,0 +1,13 @@
+let x = 100;
+
+do {
+    console.log(x / 2);
+    x = x - 5;
+} while (x >= 0);
+
+let y = 0;
+
+do {
+    console.log(`O x agora é ${y}.`);
+    y = y + 1;
+} while (y <= 10);
