6636f55dfafdfde6866b4e5a60b1669495d35701
HgPBrito <hgbrito.tr@gmail.com>
2022-05-16 18:08:39 -0300

    mais estruturas

--- 004-estrutura-de-programacao/016_break/script.js
+++ 004-estrutura-de-programacao/016_break/script.js
@@ -1,3 +1,13 @@
-for(let i = 0; i < 100; i + 3) {
-    console.log(`O i agora é ${numero}`);
+let nome = 'Hugo';
+for(let i = 0; i < 10; i + 1) {   
+
+    if(i == 3) {
+        nome = 'João';
+    }
+    if(i == 5 && nome == 'João') {
+        console.log('O nome é João no 5');
+        break;
+    }
+
+    console.log(i);
 }
