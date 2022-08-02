1d4897c1984de30c7bc2f4323ab415a9a2da146a
HgPBrito <hgbrito.tr@gmail.com>
2022-08-02 18:45:44 -0300

    ex4

---
+++ 005-exercicios-estrutura-de-programacao/004_exercicios_math/script.js
@@ -0,0 +1,16 @@
+let num = 0;
+let exp = 0;
+let resultado = 0;
+
+num = 2;
+exp = 2;
+resultado = Math.pow(num,exp);
+console.log(resultado);
+
+num = 3;
+resultado = Math.pow(num,exp);
+console.log(resultado);
+
+num = 18;
+resultado = Math.pow(num,exp);
+console.log(resultado);
