b553aaa9d9076135f9a54771abb80f0e5be609df
HgPBrito <hgbrito.tr@gmail.com>
2022-08-05 19:06:32 -0300

    ex5

---
+++ 005-exercicios-estrutura-de-programacao/005_exercicios_ifelse/script.js
@@ -0,0 +1,7 @@
+let velCarro = 40;
+const velMaxima = 80;
+if(velCarro <= velMaxima) {
+    console.log('Você está dentro do limite de velocidade!');
+} else {
+    console.log('Você está acima do limite de velocidade, diminua!');
+}
