fa78ce0c2c391d1543ac137867dd9c64925ae82c
HgPBrito <hgbrito.tr@gmail.com>
2024-01-29 18:00:33 -0300

    objetos e arrays ate push,pop,shift e unshift

--- 008-objetos-arrays/012_metodos-de-arrays-shift-e-unshift/script.js
+++ 008-objetos-arrays/012_metodos-de-arrays-shift-e-unshift/script.js
@@ -1,11 +1,11 @@
 let nomes = ['Hugo', 'Roberto', 'João', 'Maria', 'Isa'];
 
-let elementoRemovido = nomes.pop();
+let elementoRemovido = nomes.shift();
 
 console.log(elementoRemovido);
 
 console.log(nomes);
 
-nomes.push('Gabriel');
+nomes.unshift('Gabriel');
 
 console.log(nomes);
